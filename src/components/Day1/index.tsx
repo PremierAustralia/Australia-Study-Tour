936e8f40f8cc5291730d55a837bf1bfea2e9dbe3
lucas30-c <hopeful.dawn1999@gmail.com>
2024-11-08 07:38:07 +1100

    feat: Project initialisation and path alias

---
+++ src/components/Day1/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import brisbaneCityCenter from '@assets/Day1/brisbane-city-center.jpg'
+import brisbaneCityMuseum from '@assets/Day1/brisbane-city-museum.jpg'
+
+type Props = {}
+
+function Day1({}: Props) {
+  return (
+    
+      <div>
+        <h2>D1 - 周一：布里斯班探索之旅</h2>
+        <ul>
+          <li>
+            <strong>上午</strong>：抵达布里斯班，开启探索澳洲的第一站。参观布里斯班博物馆，了解澳洲的历史和文化背景。随后，漫步Southbank（南岸公园），体验布里斯班的自然与都市风光。
+          </li>
+          <li>
+            <strong>下午</strong>：布里斯班市区游览，感受这座城市的现代与自然结合，领略澳洲独特的城市氛围。
+          </li>
+          <li>
+            <strong>住宿</strong>：寄宿家庭或酒店（全程精心挑选，让你感受澳洲家庭的温暖与文化）
+          </li>
+        </ul>
+        <div>
+          <img src={brisbaneCityCenter} alt="布里斯班市中心" />
+          <img src={brisbaneCityMuseum} alt="布里斯班城市博物馆" />
+        </div>
+      </div>
+    
+  )
+}
+
+export default Day1
